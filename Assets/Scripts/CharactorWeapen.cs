bdd4950e59268c64608a40d3edb5a4278d38de93
Victormaa <389796272@qq.com>
2025-09-08 15:34:27 +0800

    update shrimp shooting

--- Assets/Scripts/CharactorWeapen.cs
+++ Assets/Scripts/CharactorWeapen.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CharactorWeapen : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public ParticleSystem particle;
     public void ShootBullet()
     {
         // 在这里实现攻击逻辑（例如生成子弹）
         var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().AddForce(Vector3.right, ForceMode.Impulse);
 
+        if(!particle.isPlaying)
+            particle.Play();
 
         Debug.Log("ShootBullet called at frame: " + Time.frameCount);
+        //Debug.Break();
+        Destroy(bullet, 5.5f);
     }
 }
