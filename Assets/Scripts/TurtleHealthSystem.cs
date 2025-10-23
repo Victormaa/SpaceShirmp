2c6f25ad13e747f314ad22e91e04f3e93a70fce8
Victormaa <389796272@qq.com>
2025-10-24 00:04:51 +0800

    upload feel

---
+++ Assets/Scripts/TurtleHealthSystem.cs
@@ -0,0 +1,28 @@
+using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleHealthSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision != null && collision.transform.tag == "ShrimpShell")
+        {
+            Destroy(collision.gameObject);
+            this.GetComponent<MMF_Player>()?.PlayFeedbacks();
+        }
+    }
+}
